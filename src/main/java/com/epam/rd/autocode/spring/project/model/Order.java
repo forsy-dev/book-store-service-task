69d8e69ad4faabc1374b610e5c86ada9094cb72b
forsy <forsst50@gmail.com>
2025-10-30 15:24:15 +0200

    feat: add Order entity

--- src/main/java/com/epam/rd/autocode/spring/project/model/Order.java
+++ src/main/java/com/epam/rd/autocode/spring/project/model/Order.java
@@ -1,5 +1,9 @@
 package com.epam.rd.autocode.spring.project.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,11 +15,33 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "ORDERS")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id", nullable = false)
+    @NotNull(message = "{NotNull.Order.client}")
     private Client client;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "employee_id", nullable = false)
+    @NotNull(message = "{NotNull.Order.employee}")
     private Employee employee;
+
+    @Column(name = "ORDER_DATE", nullable = false)
+    @NotNull(message = "{NotNull.Order.orderDate}")
+    @PastOrPresent(message = "{PastOrPresent.Order.orderDate}")
     private LocalDateTime orderDate;
+
+    @Column(name = "PRICE", nullable = false, precision = 10, scale = 2)
+    @NotNull(message = "{NotNull.Order.price}")
+    @DecimalMin(value = "0.01", message = "{DecimalMin.Order.price}")
     private BigDecimal price;
+
     private List<BookItem> bookItems;
 }
